05ad12f4745f2c69f3d6f61971c421bd99c9a972
Pushpa S <deepikackm09@gmail.com>
2026-04-14 11:03:18 +0530

    fix carousel

--- purplle-app/src/pages/CarouselPage.jsx
+++ purplle-app/src/pages/CarouselPage.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import CarouselMain from "./CarouselMain";
 
-import slide1 from "../assets/slide1.jpg"; 
+/*import slide1 from "../assets/slide1.jpg"; 
 import slide2 from "../assets/slide2.jpg"; 
 import slide3 from "../assets/slide3.jpg"; 
-const slides = [slide1, slide2, slide3]
+const slides = [slide1, slide2, slide3]*/
 
 
 function CarouselPage() {
