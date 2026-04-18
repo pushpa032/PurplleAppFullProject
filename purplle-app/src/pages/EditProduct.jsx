63b292ae74550701eb3d044795000759e9ca4b45
Pushpa S <deepikackm09@gmail.com>
2026-04-18 10:20:12 +0530

    added new in the edit page

--- purplle-app/src/pages/EditProduct.jsx
+++ purplle-app/src/pages/EditProduct.jsx
@@ -4,7 +4,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import "../styles/EditProduct.css";
 
 
-function EditProduct(){
+function EditProduct() {
     const { id } = useParams();
     const navigate = useNavigate();
 
@@ -17,99 +17,112 @@ function EditProduct(){
 
     useEffect(() => {
         axios.get(`https://purplleappbackend.onrender.com/products/${id}`)
-        .then(res => {
-            setName(res.data.name);
-            setPrice(res.data.price);
-            setCategory(res.data.category);
-            setDescription(res.data.description);
-            setRating(res.data.rating);
-            setImageUrl(res.data.imageUrl)
-        })
-        .catch(err => console.log(err));
+            .then(res => {
+                setName(res.data.name);
+                setPrice(res.data.price);
+                setCategory(res.data.category);
+                setDescription(res.data.description);
+                setRating(res.data.rating);
+                setImageUrl(res.data.imageUrl)
+            })
+            .catch(err => console.log(err));
     }, [id]);
 
     const handleUpdate = async (e) => {
         e.preventDefault();
 
-        const formData = new FormData();
+        /*const formData = new FormData();
         formData.append("name", name);
         formData.append("price", price);
         formData.append("category", category);
         formData.append("description", description);
         formData.append("rating", rating);
-        formData.append("imageUrl", imageUrl);
+        formData.append("imageUrl", imageUrl);*/
 
         /*if (image){
             formData.append("imageUrl", imageUrl);
         }*/
+        try {
+            await axios.put(`https://purplleappbackend.onrender.com/products/${id}`,
+                {
+                    name,
+                    price,
+                    category,
+                    description,
+                    rating,
+                    imageUrl,
 
-        await axios.put(`https://purplleappbackend.onrender.com/products/${id}`, formData);
-        alert("Product Updated");
-        navigate("/admin/manageProduct");
+                });
+            alert("Product Updated");
+            navigate("/admin/manageProduct");
+        }catch(error) {
+            console.log(error);
+            alert("Error uploading product");
+        }
     };
 
-    return (
-        <div className="edit-product-container">
-            <h2>Edit Product</h2>
+        return (
+            <div className="edit-product-container">
+                <h2>Edit Product</h2>
 
-            <form onSubmit={handleUpdate}>
-                <input
-                type="text"
-                value={name}
-                placeholder="Product Name"
-                onChange= { (e) => setName(e.target.value)}
-                />
+                <form onSubmit={handleUpdate}>
+                    <input
+                        type="text"
+                        value={name}
+                        placeholder="Product Name"
+                        onChange={(e) => setName(e.target.value)}
+                    />
 
-                <input
-                type="number"
-                value={price}
-                placeholder="Price"
-                onChange={(e) => setPrice(e.target.value)}
-                />
+                    <input
+                        type="number"
+                        value={price}
+                        placeholder="Price"
+                        onChange={(e) => setPrice(e.target.value)}
+                    />
 
-                <select
-                value={category}
-                onChange={(e) => setCategory(e.target.value)}
-                >
-                <option value="">Select Category</option>
-                <option value="Skincare">Skincare</option>
-                <option value="Makeup">Makeup</option>
-                <option value="Hair Care">Hair Care</option>
-                <option value="Personal Care">Personal Care</option>
-                <option value="Fragrance">Fragrance</option>
-                <option value="Bath and Body">Bath and Body</option>
-                </select>
+                    <select
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                    >
+                        <option value="">Select Category</option>
+                        <option value="Skincare">Skincare</option>
+                        <option value="Makeup">Makeup</option>
+                        <option value="Hair Care">Hair Care</option>
+                        <option value="Personal Care">Personal Care</option>
+                        <option value="Fragrance">Fragrance</option>
+                        <option value="Bath and Body">Bath and Body</option>
+                    </select>
 
-                <input
-                type="text"
-                value={description}
-                placeholder="Description"
-                onChange={(e) => setDescription(e.target.value)}
-                />
+                    <input
+                        type="text"
+                        value={description}
+                        placeholder="Description"
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
 
-                <input
-                type="number"
-                value={rating}
-                placeholder="Rating"
-                onChange={(e) => setRating(e.target.value)}
-                />
+                    <input
+                        type="number"
+                        value={rating}
+                        placeholder="Rating"
+                        onChange={(e) => setRating(e.target.value)}
+                    />
 
-                <input
-                type="text"
-                value={imageUrl}
-                placeholder="Add New Image URL"
-                onChange={(e) => setImageUrl(e.target.value)}
-                />
+                    <input
+                        type="text"
+                        value={imageUrl}
+                        placeholder="Add New Image URL"
+                        onChange={(e) => setImageUrl(e.target.value)}
+                    />
 
-                <button type="submit">
-                    Update Product
-                </button>
+                    <button type="submit">
+                        Update Product
+                    </button>
 
 
-            </form>
-        </div>
-    );
+                </form>
+            </div>
+        );
 
-}
+    }
 
-export default EditProduct;
+    export default EditProduct;
