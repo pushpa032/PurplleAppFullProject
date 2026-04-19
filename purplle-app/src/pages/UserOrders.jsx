db217e8db69c14f818adecc66383baea5255402b
Pushpa S <deepikackm09@gmail.com>
2026-04-19 21:39:00 +0530

    user order

--- purplle-app/src/pages/UserOrders.jsx
+++ purplle-app/src/pages/UserOrders.jsx
@@ -18,16 +18,25 @@ function UserOrders() {
   }, []);
 
   return (
-    <div className="orders-container">
+    <div className="user-orders-container">
       <h2>My Orders</h2>
 
       {orders.length === 0 ? (
         <p>No orders found</p>
       ) : (
         orders.map((order) => (
-          <div key={order._id} className="order-card">
+          <div key={order._id} className="user-order-card">
             <p><b>Total:</b> ₹{order.totalPrice}</p>
             <p><b>Status:</b> {order.status}</p>
+
+            <div className="user-order-products">
+                {order.products.map((p, i) => (
+                    <div key={i} className="user-order-product">
+                        <img src={p.imageUrl} alt={p.name} />
+                        <p>{p.name}</p>
+                     </div>
+                     ))}
+            </div>
           </div>
         ))
       )}
