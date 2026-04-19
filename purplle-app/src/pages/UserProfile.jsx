90a4af1a9e1f070667fd1cac4b6457859b3ae11f
Pushpa S <deepikackm09@gmail.com>
2026-04-19 21:19:32 +0530

    style

--- purplle-app/src/pages/UserProfile.jsx
+++ purplle-app/src/pages/UserProfile.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../styles/UserProfile.css";
 
 function UserProfile() {
     const [user, setUser] = useState(null);
