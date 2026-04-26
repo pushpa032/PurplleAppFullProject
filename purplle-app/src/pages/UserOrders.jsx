7be9daee1ae8dafbd0a28e26acabc72a3e1e20a2
Pushpa S <deepikackm09@gmail.com>
2026-04-26 14:30:41 +0530

    order

--- purplle-app/src/pages/UserOrders.jsx
+++ purplle-app/src/pages/UserOrders.jsx
@@ -3,43 +3,64 @@ import "../styles/UserOrders.css";
 
 function UserOrders() {
   const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  //  GET LOGGED USER
   const user = JSON.parse(localStorage.getItem("user"));
 
-  // FETCH USER ORDERS
   useEffect(() => {
     if (user) {
       fetch(`https://purplleappbackend.onrender.com/getOrders/${user.mobile}`)
         .then(res => res.json())
-        .then(data => setOrders(data.orders))
-        .catch(err => console.log(err));
+        .then(data => {
+          setOrders(data.orders);
+          setLoading(false);
+        })
+        .catch(err => {
+          console.log(err);
+          setLoading(false);
+        });
     }
   }, []);
+
+  // show loading first
+  if (loading) {
+    return (
+      <div className="user-orders-container">
+        <h2>My Orders</h2>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  // then check orders
+  if (orders.length === 0) {
+    return (
+      <div className="user-orders-container">
+        <h2>My Orders</h2>
+        <p>No orders found</p>
+      </div>
+    );
+  }
 
   return (
     <div className="user-orders-container">
       <h2>My Orders</h2>
 
-      {orders.length === 0 ? (
-        <p>No orders found</p>
-      ) : (
-        orders.map((order) => (
-          <div key={order._id} className="user-order-card">
-            <p><b>Total:</b> ₹{order.totalPrice}</p>
-            <p><b>Status:</b> {order.status}</p>
+      {orders.map((order) => (
+        <div key={order._id} className="user-order-card">
+          <p><b>Total:</b> ₹{order.totalPrice}</p>
+          <p><b>Status:</b> {order.status}</p>
 
-            <div className="user-order-products">
-                {order.products.map((p, i) => (
-                    <div key={i} className="user-order-product">
-                        <img src={p.imageUrl} alt={p.name} />
-                        <p>{p.name}</p>
-                     </div>
-                     ))}
-            </div>
+          <div className="user-order-products">
+            {order.products.map((p, i) => (
+              <div key={i} className="user-order-product">
+                <img src={p.imageUrl} alt={p.name} />
+                <p>{p.name}</p>
+              </div>
+            ))}
           </div>
-        ))
-      )}
+        </div>
+      ))}
     </div>
   );
 }
