ebe14cb1eb163644b4df980799b5a23277a35cdf
Pushpa S <deepikackm09@gmail.com>
2026-04-30 14:49:45 +0530

    mail

--- backendPurplle/controllers/sendEmail.js
+++ backendPurplle/controllers/sendEmail.js
@@ -1,28 +1,37 @@
 const nodemailer = require('nodemailer');
 
-
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  service: 'gmail',  
   auth: {
-    user: (process.env.EMAIL_USER),
-    pass: (process.env.EMAIL_PASS)
-  },
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
 });
 
-async function sendMail(to, subject, html,  attachments) {
+// debug check
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP READY");
+  }
+});
+
+async function sendMail(to, subject, html, attachments) {
   try {
+    console.log("Sending mail to:", to);
+
     await transporter.sendMail({
-      from: (process.env.EMAIL_USER),
+      from: process.env.EMAIL_USER,
       to,
       subject,
       html,
-      attachments,
-});
-    console.log("Email sent successfully to", to);
+      attachments
+    });
+
+    console.log("Email sent successfully");
   } catch (error) {
-    console.log("Email sending failed:", error);
+    console.log("Email sending failed FULL:", error);
     throw error;
   }
 }
