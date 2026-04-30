7c893ea3c7723a2b80f07fc7a6ee74c6d5f300cd
Pushpa S <deepikackm09@gmail.com>
2026-04-30 18:51:03 +0530

    yy

--- backendPurplle/controllers/sendEmail.js
+++ backendPurplle/controllers/sendEmail.js
@@ -10,8 +10,6 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
 
-  family: 4,
-
   auth: {
     user: (process.env.EMAIL_USER),
     pass: (process.env.EMAIL_PASS)
