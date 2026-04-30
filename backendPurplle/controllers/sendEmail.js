c6302ab56020c2c696967672eed9408c07951c50
Pushpa S <deepikackm09@gmail.com>
2026-04-30 15:48:04 +0530

    fix

--- backendPurplle/controllers/sendEmail.js
+++ backendPurplle/controllers/sendEmail.js
@@ -9,6 +9,9 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
+
+  family: 4,
+  
   auth: {
     user: (process.env.EMAIL_USER),
     pass: (process.env.EMAIL_PASS)
