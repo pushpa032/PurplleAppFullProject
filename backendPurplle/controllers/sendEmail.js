ec52981004d99a668fedc08c78777952b5033e8b
Pushpa S <deepikackm09@gmail.com>
2026-05-01 21:09:02 +0530

    ppppp

--- backendPurplle/controllers/sendEmail.js
+++ backendPurplle/controllers/sendEmail.js
@@ -1,4 +1,4 @@
-const nodemailer = require("nodemailer");
+/*const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
@@ -32,6 +32,39 @@ async function sendMail(to, subject, html, attachments) {
     console.log("Email sent");
   } catch (error) {
     console.log("Email failed:", error);
+  }
+}
+
+module.exports = sendMail;*/
+
+
+
+const axios = require("axios");
+
+async function sendMail(to, subject, html) {
+  try {
+    const response = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: "Purplle App",
+          email: "pushpackm09@gmail.com", 
+        },
+        to: [{ email: to }],
+        subject: subject,
+        htmlContent: html,
+      },
+      {
+        headers: {
+          "api-key": process.env.BREVO_API_KEY,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    console.log("Email sent:", response.data);
+  } catch (error) {
+    console.log("Mail error:", error.response?.data || error.message);
   }
 }
 
