db37a2114e3a274fbc2d67f939aff980ccf57a3f
Pushpa S <deepikackm09@gmail.com>
2026-05-01 20:27:22 +0530

    error clear

--- backendPurplle/controllers/sendEmail.js
+++ backendPurplle/controllers/sendEmail.js
@@ -6,9 +6,10 @@ const nodemailer = require('nodemailer');
 
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp-relay.brevo.com',
-  port: 587,
-  secure: false,
+  //host: 'gmail',
+  service:"gmail",
+  //port: 587,
+  //secure: false,
 
   auth: {
     user: (process.env.EMAIL_USER),
